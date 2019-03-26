0ebe4008dcef07f9b57f7aed98840a447f5bd23f
Victor Karlsson <victor.karlsson@elev.ga.ntig.se>
2019-03-26 16:00:24 +0100

    Mindre designändringar

--- js/homepage.js
+++ js/homepage.js
@@ -4,3 +4,11 @@ function myscroll() {
     scrollTop: $("#bottomtext").offset().top
   }, 1000);
 }
+
+/*$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+    var height = $(window).height();
+    $('.btn-success, .slogan').css({
+        'opacity': ((height - scrollTop) / height)
+    });
+});*/
